5d4c7caa7ea478a3107732cb1a68ac243742e2b4
Vandana  kumari <vandanasinghbhore04@gmail.com>
2026-07-22 00:03:28 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (99.15%) - LeetSync

---
+++ 58-length-of-last-word/length-of-last-word.java
@@ -0,0 +1,21 @@
+class Solution {
+
+    public int lengthOfLastWord(String s) {
+
+        int count = 0;
+        int i = s.length() - 1;
+
+        // Skip trailing spaces
+        while (i >= 0 && s.charAt(i) == ' ') {
+            i--;
+        }
+
+        // Count characters of the last word
+        while (i >= 0 && s.charAt(i) != ' ') {
+            count++;
+            i--;
+        }
+
+        return count;
+    }
+}
