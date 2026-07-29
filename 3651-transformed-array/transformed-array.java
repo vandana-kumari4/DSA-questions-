d148574b951980495c85a77d2e7936cb380a2d71
Vandana  kumari <vandanasinghbhore04@gmail.com>
2026-07-29 23:36:10 +0530

    Time: 1 ms (98.20%) | Memory: 46.6 MB (77.40%) - LeetSync

---
+++ 3651-transformed-array/transformed-array.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] constructTransformedArray(int[] nums) {
+        int n = nums.length;
+        int[] result = new int[n];
+        
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == 0) {
+                result[i] = 0;
+            } else {
+                // Calculate new index with circular wrapping
+                int newIndex = ((i + nums[i]) % n + n) % n;
+                result[i] = nums[newIndex];
+            }
+        }
+        
+        return result;
+    }
+}
