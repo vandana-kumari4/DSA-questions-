322d2e8ca7328038102e146b5a4b5f1cadaf9997
Vandana  kumari <vandanasinghbhore04@gmail.com>
2026-07-31 23:50:51 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (16.06%) - LeetSync

---
+++ 225-implement-stack-using-queues/implement-stack-using-queues.java
@@ -0,0 +1,52 @@
+import java.util.*;
+
+class MyStack {
+
+    Queue<Integer> q1;
+    Queue<Integer> q2;
+
+    public MyStack() {
+        q1 = new LinkedList<>();
+        q2 = new LinkedList<>();
+    }
+
+    public void push(int x) {
+        q1.offer(x);
+    }
+
+    public int pop() {
+
+        while(q1.size() > 1){
+            q2.offer(q1.poll());
+        }
+
+        int ans = q1.poll();
+
+        Queue<Integer> temp = q1;
+        q1 = q2;
+        q2 = temp;
+
+        return ans;
+    }
+
+    public int top() {
+
+        while(q1.size() > 1){
+            q2.offer(q1.poll());
+        }
+
+        int ans = q1.peek();
+
+        q2.offer(q1.poll());
+
+        Queue<Integer> temp = q1;
+        q1 = q2;
+        q2 = temp;
+
+        return ans;
+    }
+
+    public boolean empty() {
+        return q1.isEmpty();
+    }
+}
