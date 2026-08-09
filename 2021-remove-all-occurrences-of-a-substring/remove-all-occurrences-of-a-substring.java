336fe6a156402e74531bda0e23e36f5a19a2c339
Vandana  kumari <vandanasinghbhore04@gmail.com>
2026-08-09 23:09:18 +0530

    Time: 1 ms (99.88%) | Memory: 44.2 MB (63.17%) - LeetSync

---
+++ 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String removeOccurrences(String s, String part) {
+
+        while (s.indexOf(part) != -1) {
+            int idx = s.indexOf(part);
+
+            s = s.substring(0, idx) +
+                s.substring(idx + part.length());
+        }
+
+        return s;
+    }
+}
