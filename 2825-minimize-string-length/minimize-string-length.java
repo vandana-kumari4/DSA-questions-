9fa38789502ecfbc5fff540088544f34ef93eed6
Vandana  kumari <vandanasinghbhore04@gmail.com>
2026-09-18 23:47:35 +0530

    Time: 3 ms (99.83%) | Memory: 46.4 MB (86.17%) - LeetSync

---
+++ 2825-minimize-string-length/minimize-string-length.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int minimizedStringLength(String s) {
+        boolean[] seen = new boolean[26];
+        int count = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            int index = s.charAt(i) - 'a';
+
+            if (!seen[index]) {
+                seen[index] = true;
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
